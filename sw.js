faa803b62b551d4211d99ddff8e5d3e30bb5c906
JonatasElias <jonatas.santos96@etec.sp.gov.br>
2020-11-06 12:32:08 -0300

    Commit 6

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('install', function(event){
-    // iniciaria os passos para instalação do nosso service worker...
-})
+var cacheName = 'NameApp-v';
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+self.addEventListener('install', function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(response){
+            return responde || fetch(e.request);
+        })
+    );
+});
